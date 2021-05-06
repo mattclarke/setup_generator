02784784f91f7f1062107b69ddd9b63bc104740d
Matt Clarke <matt.clarke@esss.se>
2021-05-06 11:29:45 +0200

    pyqt5 basics added

---
+++ main.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import QApplication, QWidget
+import sys
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = QWidget()
+    window.show()
+    app.exec_()
